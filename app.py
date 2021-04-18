0c07b058c2bbcaa378e76968d7a60a6a76f29cd1
Carter Hinsley <carterhinsley@outlook.com>
2021-04-18 00:30:00 +0000

    User interface prototype

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, make_response, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 interviewees = {
-    "albert einstein",
-    "bernhard riemann",
-    "isaac newton",
+    "Albert Einstein",
+    "Bernhard Riemann",
+    "Isaac Newton",
 }
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST"])
 def index():
-    if "interviewee" not in request.args or request.args["interviewee"] not in interviewees:
+    if "interviewee" not in request.form or request.form["interviewee"] not in interviewees:
+        # No interviewee was supplied. Who are we interviewing?
+        # Warning: This logic is totally broken/untested. Don't rely on it.
         print(url_for("select"))
         return redirect(url_for("select"))
     else:
-        return render_template("index.html")
+        response = make_response()
+
+        if "content" not in request.cookies or "message" not in request.form:
+            # We're just starting the interview. The check for "message" in POST data
+            # is crucial in case the "content" cookie was present by accident.
+            content = f"The following is an excerpt of a transcript from an informal interview with {request.form['interviewee']}.\n\nInterviewer: "
+        else:
+            content = request.cookies.get("content")
+            # The interview has already begun.
+            if "summarize" in request.form:
+                # No message was sent -- this is just a request to summarize the existing transcript.
+                pass
+            else:
+                # A new message was sent.
+                content += request.form["message"] + "\n\nInterviewer: "
+        
+        response.set_cookie("content", content)
+
+        response.set_data(render_template("index.html", content=content))
+
+        return response
 
 @app.route("/select")
 def select():
